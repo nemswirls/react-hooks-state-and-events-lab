b61818763b9a3436117b8bdfe7088d3c558adf4b
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-11-24 23:29:52 -0500

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+
+  const toggleCart = () => {
+    setInCart(prevInCart => !prevInCart)
+  }
   return (
-    <li className="">
+    <li className={inCart ? 'in-cart' : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleCart}> {inCart ? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
-  );
+    );
 }
 
 export default Item;
